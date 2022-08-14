13b98a4bd2a624860be3374d58d53e2739b7ba04
Наталья <109668015+andre19071@users.noreply.github.com>
2022-08-14 19:34:53 +0700

    Добавила условия задач

--- task17/Program.cs
+++ task17/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 17. Напишите программу, которая принимает на 
+//вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и 
+//выдаёт номер четверти плоскости, в которой находится эта точка.
