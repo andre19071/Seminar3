0da1fc131c048ada5256b0a7656f3ca626cb9781
Наталья <109668015+andre19071@users.noreply.github.com>
2022-08-16 13:34:50 +0700

    task22

--- task22/Program.cs
+++ task22/Program.cs
@@ -2,3 +2,11 @@
 // число (N) и выдаёт таблицу квадратов чисел от 1 до N.
 //5 -> 1, 4, 9, 16, 25.
 //2 -> 1,4
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+{
+    Console.WriteLine( i*i + " " );
+}
